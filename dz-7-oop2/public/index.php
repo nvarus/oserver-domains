586ba75561012456378873a9fa0fe42d0cd9f144
nvaru <varyukhin.alx@gmail.com>
2023-05-02 20:55:25 +0400

    deg

--- dz-7-oop2/public/index.php
+++ dz-7-oop2/public/index.php
@@ -1,26 +1,30 @@
 <?php
-
-include_once '../src/Category/Category.php';
-include_once '../src/Category/MonitorCategory.php';
-include_once '../src/Category/PhoneCategory.php';
-
-$Phones = new PhoneCategory("Phones");
-$Phones->setListProducts([
-	["Name" => "iPhone 13", "Price" => 73790, "Description" => "Phone", "Brand" => "Apple", "CPU" => "Apple A15 Bionic", "RAM" => 4, "CountSIM" => 3, "HDD" => 128, "OS" => "IOS"],
-	["Name" => "Galaxy S23", "Price" => 77490, "Description" => "Phone", "Brand" => "Samsung", "CPU" => "Qualcomm Snapdragon 8 Gen 2", "RAM" => 8, "CountSIM" => 1, "HDD" => 256, "OS" => "Android"],
-	["Name" => "11 Light 5G", "Price" => 28990, "Description" => "Phone", "Brand" => "Xiaomi", "CPU" => "Snapdragon 778G", "RAM" => 8, "CountSIM" => 2, "HDD" => 128, "OS" => "Android"],
-]);
-$Monitors = new MonitorCategory("Monitors");
-$Monitors->setListProducts([
-	["Name" => "MateView SE SSN-24", "Price" => 10990, "Description" => "Monitor", "Brand" => "Huawei", "Diagonal" => 23.8, "Frequency" => 75],
-	["Name" => "P2723DE", "Price" => 48790, "Description" => "Monitor", "Brand" => "DELL", "Diagonal" => 27, "Frequency" => 60],
-	["Name" => "34WP65C-B", "Price" => 35550, "Description" => "Monitor", "Brand" => "LG", "Diagonal" => 34, "Frequency" => 160],
-]);
-
-include_once '../src/ListCategory/ListCategory.php';
-$listCategory = new ListCategory([$Phones, $Monitors]);
-
-
+	include_once '../src/Category/Category.php';
+	include_once '../src/Category/MonitorCategory.php';
+	include_once '../src/Category/PhoneCategory.php';
+	
+	$Phones = new PhoneCategory("Phones");
+	$Phones->setListProducts([
+		["Name" => "iPhone 13", "Price" => 73790, "Description" => "Phone", "Brand" => "Apple",
+			"CPU" => "Apple A15 Bionic", "RAM" => 4, "CountSIM" => 3, "HDD" => 128, "OS" => "IOS"],
+		["Name" => "Galaxy S23", "Price" => 77490, "Description" => "Phone", "Brand" => "Samsung",
+			"CPU" => "Qualcomm Snapdragon 8 Gen 2", "RAM" => 8, "CountSIM" => 1, "HDD" => 256, "OS" => "Android"],
+		["Name" => "11 Light 5G", "Price" => 28990, "Description" => "Phone", "Brand" => "Xiaomi",
+			"CPU" => "Snapdragon 778G", "RAM" => 8, "CountSIM" => 2, "HDD" => 128, "OS" => "Android"],
+	]);
+	
+	$Monitors = new MonitorCategory("Monitors");
+	$Monitors->setListProducts([
+		["Name" => "MateView SE SSN-24", "Price" => 10990, "Description" => "Monitor", "Brand" => "Huawei",
+			"Diagonal" => 23.8, "Frequency" => 75],
+		["Name" => "P2723DE", "Price" => 48790, "Description" => "Monitor", "Brand" => "DELL", "Diagonal" => 27,
+			"Frequency" => 60],
+		["Name" => "34WP65C-B", "Price" => 35550, "Description" => "Monitor", "Brand" => "LG", "Diagonal" => 34,
+			"Frequency" => 160],
+	]);
+	
+	include_once '../src/ListCategory/ListCategory.php';
+	$listCategory = new ListCategory([$Phones, $Monitors]);
 ?>
 
 <!doctype html>
@@ -32,41 +36,53 @@ $listCategory = new ListCategory([$Phones, $Monitors]);
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Document</title>
 	<style>
-		.category {
-			 cursor: pointer;
-			 list-style-type: none;
-			 line-height: 30px;
-			 font-size: 18px;
-		    font-weight: 600;
-			 width: 200px;
-			 text-align: center;
-			 border: 1px solid teal;
-			 margin: 10px;
-		}
-		.category:hover {
-			background: teal;
-			color: white;
+	   .category {
+		   cursor: pointer;
+		   list-style-type: none;
+		   line-height: 30px;
+		   font-size: 18px;
+		   font-weight: 600;
+		   width: 200px;
+		   text-align: center;
+		   border: 1px solid teal;
+		   margin: 10px;
+	   }
 
-		}
-		.category:active {
-			transform: scale(0.95);
-		}
-		.filter__name {
-			margin: 5px;
-		}
-		.filter__input {
-			line-height: 20px;
-			text-align: center;
-			margin: 5px 0 5px 0;
-		}
-		.apply-btn {
-			border: 1px solid teal;
-			margin-left: 5px;
-			line-height: 20px;
-		}
-		.apply-btn:hover {
-			background: teal;
-			color: white;
+	   .category:hover {
+		   background: teal;
+		   color: white;
+
+	   }
+
+	   .category:active {
+		   transform: scale(0.95);
+	   }
+
+	   .filter__name {
+		   margin: 5px;
+	   }
+
+	   .filter__input {
+		   line-height: 20px;
+		   text-align: center;
+		   margin: 5px 0 5px 0;
+	   }
+
+	   .apply-btn {
+		   border: 1px solid teal;
+		   margin-left: 5px;
+		   line-height: 20px;
+	   }
+
+	   .apply-btn:hover {
+	      background: teal;
+	      color: white;
+      }
+	   .products {
+		    font-weight: 600;
+	      font-size: 20px;
+		    margin-bottom: 10px;
+	   }
 	</style>
 </head>
 <body>
@@ -79,11 +95,11 @@ $listCategory = new ListCategory([$Phones, $Monitors]);
 		echo "<div><button class='category' name='category' value='$category'>$category</button></div>";
 	}
 	echo "<form>";
-
+	
 	// событие при нажатии кнопки категории
 	if (!empty($_POST['category'])) {
 		$cat = $_POST['category'];
-
+		
 		// вызываем метод getName
 		foreach ($listCategory->categoryList as $object) {
 			if ($object->getName() == $cat) {
@@ -91,7 +107,7 @@ $listCategory = new ListCategory([$Phones, $Monitors]);
 			}
 		}
 	}
-
+	
 	// ловил значения выбранных фильтров
 	if (isset($_POST['cat'])) {
 		$cat = $_POST['cat'];
@@ -103,8 +119,8 @@ $listCategory = new ListCategory([$Phones, $Monitors]);
 				foreach ($item->getFilters() as $filter) {
 					$filter_list[$filter] = $_POST[$filter];
 				}
-			// вызываем applyFilters для вывода отфильтрованных значений
-			$item->applyFilters($filter_list);
+				// вызываем applyFilters для вывода отфильтрованных значений
+				$item->applyFilters($filter_list);
 			}
 		}
 	}
--- dz-7-oop2/src/Category/Category.php
+++ dz-7-oop2/src/Category/Category.php
@@ -1,60 +1,83 @@
 <?php
-
-class Category
-{
-	private string $name;
-	public array $listProducts;
-	protected array $filters;
 	
-	function __construct(string $_name, $_price = "Price")
+	class Category
 	{
-		$this->name = $_name;
-		$this->filters[] = $_price;
+		private string $name;
+		public array $listProducts;
+		protected array $filters;
+		
+		function __construct(string $_name, $_price = "Price")
+		{
+			$this->name = $_name;
+			$this->filters[] = $_price;
+		}
+		
+		public function getName(): string
+		{
+			return $this->name;
+		}
+		
+		/** Вводим названия продуктов  */
+		public function setListProducts(array $listProducts): void
+		{
+			$this->listProducts = $listProducts;
+		}
+		
+		/** Получить массив с фильтрами */
+		public function getFilters(): array
+		{
+			return $this->filters;
+		}
+		
+		/** Выводим на экран фильтры */
+		public function showFilters()
+		{
+			echo "</form>";
+			echo "<form action='' method='post'>";
+			echo "<input type='hidden' name='cat' value='$this->name'>";
+			foreach ($this->filters as $item) {
+				echo "<span class='filter__name'><b>$item</b></span>";
+				$placeholder_list = [];
+				foreach ($this->listProducts as $product) {
+					foreach ($product as $key => $value) {
+						if ($item == $key) {
+							$placeholder_list[] = $value;
+						}
+					}
+				}
+				sort($placeholder_list);
+				$first_value = $placeholder_list[0];
+				$last_value = $placeholder_list[array_key_last($placeholder_list)];
+				echo "<input class='filter__input' type='text' name='$item' placeholder='min: $first_value, max: $last_value'>";
+				
+			}
+			echo "<button type='submit' class='apply-btn'>Применить</button>";
+			echo "</form>";
+			
+		}
+		
+		/** Отображаем отфильтрованный список товаров */
+		public function applyFilters($filters)
+		{
+			// перебираем список товаров
+			foreach ($this->listProducts as $product) {
+				// перебираем список фильтров
+				$flag = true;
+				foreach ($filters as $filter_key => $filter_value) {
+					// если находим несовпадающий фильтр, $flag = false
+					if ($filter_value != '' and $filter_value != $product[$filter_key]) {
+						$flag = false;
+						break;
+					}
+				}
+				// выводим товар на экран, только если $flag == true
+				if ($flag) {
+					echo "<div class='products'>";
+					foreach ($product as $key => $value) {
+						echo " $key: $value ";
+					}
+					echo "</div>";
+				}
+			}
+		}
 	}
-	
-	public function getName():string {
-		return $this->name;
-	}
-	
-	/** Вводим названия продуктов  */
-	public function setListProducts(array $listProducts): void
-	{
-		$this->listProducts = $listProducts;
-	}
-	
-	public function getFilters():array
-	{
-		return $this->filters;
-	}
-
-	public function showFilters() {
-            echo "</form>";
-            echo "<form action='' method='post'>";
-            echo "<input type='hidden' name='cat' value='$this->name'>";
-		foreach ($this->filters as $item) {
-            echo "<span class='filter__name'><b>$item</b></span>";
-            $placeholder_list = [];
-            foreach ($this->listProducts as $product) {
-                foreach ($product as $key=>$value) {
-                    if ($item == $key) {
-                        $placeholder_list[] = $value;
-                    }
-                }
-            }
-            sort($placeholder_list);
-            $first_value = $placeholder_list[0];
-            $last_value = $placeholder_list[array_key_last($placeholder_list)];
-            echo "<input class='filter__input' type='text' name='$item' placeholder='min: $first_value, max: $last_value'>";
-
-        }
-        echo "<button type='submit' class='apply-btn'>Применить</button>";
-        echo "</form>";
-
-	}
-
-    public function applyFilters($filters) {
-        echo "<pre>";
-        print_r($filters);
-        echo "</pre>";
-    }
-}
